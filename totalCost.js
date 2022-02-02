5dfb5e65a20881461f2478072fbbcccc626e315f
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-02 18:47:36 +0600

    total cost from shopping cart

---
+++ totalCost.js
@@ -0,0 +1,31 @@
+// Calculating total cost from array of objects
+
+const products = [{
+        item: 'laptop',
+        price: 50000,
+        quantity: 1
+    },
+    {
+        item: 'mobile',
+        price: 30000,
+        quantity: 1
+    },
+    {
+        item: 'gimble',
+        price: 10000,
+        quantity: 1
+    },
+    {
+        item: 'camera',
+        price: 20000,
+        quantity: 1
+    }
+];
+let totalPrice = 0;
+let totalQuantity = 0;
+
+for (const product of products) {
+    totalPrice += product.price;
+    totalQuantity += product.quantity;
+}
+console.log('Total price: ', totalPrice, ' Total quantity: ', totalQuantity);
