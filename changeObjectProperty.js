85efb7e5ec60256e917ed5f8052ee939ac265832
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-03 16:59:47 +0600

    feet to inch function

---
+++ changeObjectProperty.js
@@ -0,0 +1,11 @@
+//Changing the properties of an object
+
+let car = {
+    name: 'Toyota',
+    color: 'black',
+    price: 2000000
+}
+console.log(car);
+
+car.price = 1500000;
+console.log(car);
---
+++ feetToInch.js
@@ -0,0 +1,6 @@
+//Function that will convert from feet to inch
+function feetToInch(feet) {
+    let inch = feet * 12;
+    return inch;
+}
+console.log(feetToInch(10));
